13c615f7b912d9feebac511ea6602ba9e3b54ffb
David <spteam.coder@gmail.com>
2024-04-09 18:20:10 +0700

    first commit

---
+++ doctor/controller/doctor.controller.js
@@ -0,0 +1,24 @@
+﻿const express = require('express');
+const router = express.Router();
+const doctorService = require('../service/doctor.service');
+
+// routes
+
+router.get('/', getAll);
+router.get('/:id', getById);
+
+module.exports = router;
+
+// route functions
+
+function getAll(req, res, next) {
+    doctorService.getAll()
+        .then(doctors => res.json(doctors))
+        .catch(next);
+}
+
+function getById(req, res, next) {
+    doctorService.getById(req.params.id)
+        .then(doctor => res.json(doctor))
+        .catch(next);
+}
